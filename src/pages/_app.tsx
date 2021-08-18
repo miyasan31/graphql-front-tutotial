46aabf7bea90b6b245438107ed694eaf8bb73928
miyasan_0301 <miyasan.19990301@gmail.com>
2021-08-18 16:32:22 +0900

    🔧 update:ts

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import "src/styles/globals.css";
+
+import { ApolloProvider } from "@apollo/client";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import { client } from "src/apollo/apollo-client";
+
+const MyApp: NextPage<AppProps> = (props) => {
+	return (
+		<ApolloProvider client={client}>
+			<props.Component {...props.pageProps} />
+		</ApolloProvider>
+	);
+};
+
+// eslint-disable-next-line import/no-default-export
+export default MyApp;
